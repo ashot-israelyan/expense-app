9f4acc1be2b74fa40c19463721786bc70686cead
Ashot Israelyan <ashot.israelyan@picsart.com>
2017-10-04 16:16:09 +0400

    Dummy data deleted

--- src/app.js
+++ src/app.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 45 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109 }));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter/>
